1996780c3aa27bb1ef876d3e6a8dd7f7a930f2b9
dcuthell <dcc1447@gmail.com>
2017-09-15 10:38:44 -0700

    add basic api call for docs in pdx

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,15 +1,19 @@
 const apiKey = require('./../.env').apiKey;
 
-$(document).ready(function(event){
-  $("#symptom-form")
-  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=anxiety&location=or-portland&user_key=${apiKey}`).then(function(response) {
-    $('#blah').text(`First doc is ${response.data[0].practices[0].name}`);
-    let i;
-    for(i in response.data){
-      $('#doc-list').append(`<li>${response.data[i].profile.first_name}</li>`);
-    }
-  }).fail(function(error) {
-    $('#blah').text(`NOOOOOOO`);
-  });
+$(document).ready(function(){
+  $("#symptom-form").submit(function(event){
+    event.preventDefault();
+    let query = $("#symptom-input").val();
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=or-portland&user_key=${apiKey}`).then(function(response) {
+      $('#blah').text(`First doc is ${response.data[0].practices[0].name}`);
+      let i;
+      for(i in response.data){
+        $('#doc-list').append(`<li>${response.data[i].profile.first_name}</li>`);
+      }
+    }).fail(function(error) {
+      $('#blah').text(`NOOOOOOO`);
+    });
+  })
+
 
 });
