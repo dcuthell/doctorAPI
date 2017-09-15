3781bc58ae1489815231c590f6f6dd6cafbd63d4
dcuthell <dcc1447@gmail.com>
2017-09-15 12:03:45 -0700

    expand api search

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -5,15 +5,23 @@ $(document).ready(function(){
     event.preventDefault();
     let query = $("#symptom-input").val();
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=or-portland&user_key=${apiKey}`).then(function(response) {
-      $('#blah').text(`First doc is ${response.data[0].practices[0].name}`);
-      let i;
-      for(i in response.data){
-        $('#doc-list').append(`<li>${response.data[i].profile.first_name}</li>`);
+      if(response.data.length == 0){
+        $('#blah').text(`No doctors found for your search of ${query}, please retry search!`);
+      }else{
+        $('#blah').text(`First doc is ${response.data[0].practices[0].name}`);
+        let docInfoString = "";
+        for(let i in response.data){
+          docInfoString += `<li>
+                              <ul>
+                                <li>Doctor Name: ${response.data[i].profile.first_name} ${response.data[i].profile.last_name} ${response.data[i].profile.title}</li>
+                                <li>Specialty: ${response.data[i].specialties[0].name}</li>
+                              </ul>
+                            </li>`;
+        }
+        $('#doc-list').html(docInfoString);
       }
     }).fail(function(error) {
-      $('#blah').text(`NOOOOOOO`);
+      $('#blah').text(error.responseText);
     });
-  })
-
-
+  });
 });
