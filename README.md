cd79808b6e74d6bb204a02da2e27dfe673e2ae08
dcuthell <dcuthell@pdx.edu>
2017-09-15 16:30:37 -0700

    add functionality, readme

---
+++ README.md
@@ -0,0 +1,51 @@
+# _The Doctor API_
+
+#### _The Doctor API will provide information on Portland Doctors. 9/15/17_
+
+#### By _**David Cuthell**_
+
+## Description
+
+_The Doctor API site allows users to get information on doctors in Portland. The user may look up a list of doctors by keyword or by last name. Users may choose to see more details for a specific doctor if they please_
+
+## Setup/Installation Requirements
+
+* _Navigate to the root directory in Terminal_
+* _Run npm init_
+* _Run bower init_
+* _Run gulp build --production_
+* _Open index.html_
+* _Enjoy!_
+
+## Specs
+
+* It will provide a list of doctors given a search word.
+  * __Example Input:__ "back pain"
+  * __Example Output:__ Doctor List
+* It will provide a list of doctors given a last name.
+  * __Example Input:__ "Smith"
+  * __Example Output:__ List of Doctors with last name Smith
+
+
+## Known Bugs
+
+_UI is incomplete_
+
+## Support and contact details
+
+_For technical support, additional information, or feedback, wait._
+
+## Technologies Used
+
+* _Git_
+* _HTML_
+* _JavaScript_
+* _jQuery_
+* _Node.js_
+* _Postman_
+
+### License
+
+*This product is licensed under the MIT license*
+
+Copyright (c) 2017 **_David Cuthell_**
--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,27 +1,101 @@
 const apiKey = require('./../.env').apiKey;
 
 $(document).ready(function(){
+
   $("#symptom-form").submit(function(event){
     event.preventDefault();
     let query = $("#symptom-input").val();
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=or-portland&user_key=${apiKey}`).then(function(response) {
       if(response.data.length == 0){
-        $('#blah').text(`No doctors found for your search of ${query}, please retry search!`);
+        $('#result-header').text(`No doctors found for your search of ${query}, please retry search!`);
       }else{
-        $('#blah').text(`First doc is ${response.data[0].practices[0].name}`);
+        $('#result-header').text(`Your search for ${query} has the following results`);
         let docInfoString = "";
         for(let i in response.data){
           docInfoString += `<li>
                               <ul>
                                 <li>Doctor Name: ${response.data[i].profile.first_name} ${response.data[i].profile.last_name} ${response.data[i].profile.title}</li>
                                 <li>Specialty: ${response.data[i].specialties[0].name}</li>
+                                <button type="button" id="${response.data[i].uid}" class="btn btn-info doc-button">Details</button>
                               </ul>
                             </li>`;
         }
         $('#doc-list').html(docInfoString);
       }
     }).fail(function(error) {
-      $('#blah').text(error.responseText);
+      $('#result-header').text(error.responseText);
     });
+  });
+
+  $("#name-form").submit(function(event){
+    event.preventDefault();
+    let hiDoc = function(){
+      alert("hello doctor");
+    };
+    let query = $("#name-input").val();
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?last_name=${query}&location=or-portland&user_key=${apiKey}`).then(function(response) {
+      if(response.data.length == 0){
+        $('#result-header').text(`No doctors found for your search of ${query}, please retry search!`);
+      }else{
+        $('#result-header').text(`Your search for ${query} has the following results`);
+        let docInfoString = "";
+        for(let i in response.data){
+          docInfoString += `<li>
+                              <ul>
+                                <li>Doctor Name: ${response.data[i].profile.first_name} ${response.data[i].profile.last_name} ${response.data[i].profile.title}</li>
+                                <li>Specialty: ${response.data[i].specialties[0].name}</li>
+                                <button type="button" id="${response.data[i].uid}" class="btn btn-info doc-button">Details</button>
+                              </ul>
+                            </li>`;
+        }
+        $('#doc-list').html(docInfoString);
+      }
+    }).fail(function(error) {
+      $('#result-header').text(error.responseText);
+    });
+  });
+
+  $('#doc-list').on('click', '.doc-button', function() {
+    let uid = $(this).attr('id');
+    $("#doc-list").hide();
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors/${uid}?user_key=${apiKey}`).then(function(response) {
+      $('#result-header').text(`Here is some more detailed information on ${response.data.profile.first_name} ${response.data.profile.last_name}`);
+      let docInfoString = `<li>Doctor Name: ${response.data.profile.first_name} ${response.data.profile.last_name} ${response.data.profile.title}</li>
+                            <li>Specialty: ${response.data.specialties[0].name}</li>`;
+      for(let i in response.data.practices){
+        if(i > 0){
+          if(response.data.practices[i].name == response.data.practices[i-1].name){
+            continue;
+          }
+        }
+        let phone = "";
+        for(let j in response.data.practices[i].phones){
+          if(response.data.practices[i].phones[j].type == "landline"){
+            phone = response.data.practices[i].phones[j].number;
+          }
+        }
+        let suite = '';
+        if(response.data.practices[i].visit_address.street2 != null){
+          suite = response.data.practices[i].visit_address.street2;
+        }
+        docInfoString += `<li>
+                            <ul>
+                              <li>Practice Name: ${response.data.practices[i].name}</li>
+                              <li>Address: ${suite} ${response.data.practices[i].visit_address.street}, ${response.data.practices[i].visit_address.zip} </li>
+                              <li>Phone: ${phone}</li>
+                              <li>Accepts New Patients: ${response.data.practices[i].accepts_new_patients}</li>
+                            </ul>
+                          </li>`;
+      }
+      docInfoString += '<button type="button" id="return" class="btn btn-info">Return</button>';
+      $('#details-list').html(docInfoString);
+    }).fail(function(error) {
+      $('#result-header').text(error.responseText);
+    });
+  });
+
+  $('#details-list').on('click', '#return', function() {
+    $('#details-list').hide();
+    $('#doc-list').show();
   });
 });
